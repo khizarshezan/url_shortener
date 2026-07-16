7f6e3d33bdbae39e5a6468c25cb169f90522c675
khizarshezan <shezankhizar@gmail.com>
2026-07-16 17:00:01 +0530

    Change app.run to use environment PORT variable

--- app.py
+++ app.py
@@ -135,5 +135,4 @@ def delete_url(id):
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
